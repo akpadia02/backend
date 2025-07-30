6c87f727f9a780ce70da15dc983dd66a2daa1d73
akpadia02 <akpadia02@gmail.com>
2025-07-30 11:51:24 +0530

    mongodb connect

---
+++ project_youtube/src/index.js
@@ -0,0 +1,30 @@
+// require('dotenv').config({path: './env'});
+import dotenv from 'dotenv';
+dotenv.config({path: './env'}); //better version
+
+// import mongoose from 'mongoose';
+// import { DB_NAME } from './contents';
+import express from 'express';
+import connectDB from './db/index.js';
+const app=express();
+
+
+//effing
+// (async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",()=>{
+//             console.log("Error",error);
+//             throw error;
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         });
+//     }catch(error){
+//         console.error('Error connecting to MongoDB:', error);
+//     }
+// })()
+
+
+
+connectDB();
